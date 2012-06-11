1b9e140e2e36d7e6bb8ca272fce659ef0c23bde8
caleb njoroge <clbnjoroge@gmail.com>
2012-06-11 18:34:00 +0300

    updates

--- projects/forms.py
+++ projects/forms.py
@@ -1,1 +1,11 @@
+from django.forms import forms
+
 __author__ = 'caleb'
+
+
+
+class SearchForm(forms.Form):
+    query=forms.CharField(
+        label=u'Enter the keyword you would like to search for',
+        widget= forms.TextInput(attrs={'size':32})
+    )
--- projects/views.py
+++ projects/views.py
@@ -6,23 +6,46 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import context
 from django.template.context import RequestContext
-from projects.models import Projects, ProjectForm
+from projects.models import Projects, ProjectForm, Project_categories
+from taggit.models import Tag
 
-
+@login_required()
 def create_project(request,):
 
     if request.method == 'POST':
-        formset = ProjectForm(request.POST, request.FILES)
+        formset = ProjectForm(request.POST, request.FILES,)
+
         if formset.is_valid():
-            formset.save()
+
+
+            project,created = Projects.objects.get_or_create(
+                client_name= request.user,
+                category = formset.cleaned_data['category']
+            )
+            project.title = formset.cleaned_data['title']
+            project.description = formset.cleaned_data['description']
+            project.date_created = formset.cleaned_data['date_created']
+            project.suggested_budget = formset.cleaned_data['suggested_budget']
+
+
+            if not created:
+                project.tags.clear()
+
+            # Create new tag list.
+            tag_names = formset.cleaned_data['tags']
+            for tag_name in tag_names:
+                tag, dummy = Tag.objects.get_or_create(name=tag_name)
+                project.tags.add(tag)
+
+            project.save()
             return HttpResponseRedirect(reverse('success'))
 
     else:
         formset = ProjectForm()
     return render_to_response("projects/create_projects.html", {
-        "formset": formset,
+        "formset": formset,'user':request.user
 
-        }, context_instance = RequestContext(request))
+        }, context_instance = RequestContext(request), )
 create_project = login_required(create_project)
 
 
@@ -34,6 +57,6 @@ def edit_project(request):
 def project_detail(request):
     pass
 
-
+@login_required()
 def project_creation_success(request):
-    return render_to_response('projects/success.html')
+    return render_to_response('projects/success.html', {'user':request.user})
